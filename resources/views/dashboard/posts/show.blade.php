f3dfb28c3f435179805a8bb9803b34cc048e83ea
adawiyahajr <adawiyahajr@gmail.com>
2023-11-24 12:49:26 +0700

    #18 Create post form

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,22 @@
+    @extends('dashboard.layoutf.main')
+
+    @section('container')
+    <div class="row justify-content-center my-3">
+        <div class="col-lg-8">
+            <form action="/posts">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                @if (request('author'))
+                    <input type="hidden" name="author"   value="{{ request('author') }}">
+                @endif
+                <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+                <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span>Back to all my post</a>
+                    <a href="" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
+                        <a href="" class="btn btn-danger"><span data-feather="x-circle"></span>Delete</a>
+                </div>
+            </form>
+        </div>
+    </div>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardPostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -47,7 +48,18 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->name('login')->middleware('guest');
 
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::get('dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)
+->middleware('auth');
