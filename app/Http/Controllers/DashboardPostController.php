1343862f5733422930c7d68b8d7802bdb0697061
adawiyahajr <adawiyahajr@gmail.com>
2023-11-25 12:49:27 +0700

    #22 Preview, update & delete image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str; // Corrected the case of 'illuminate'
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -84,6 +85,7 @@ class DashboardPostController extends Controller
         $rules = $request->validate([
             'title' => 'required|max:255',
             'category' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ]);
 
@@ -92,6 +94,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules);
+
+        if($request->file('image')) {
+            if($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -107,8 +116,10 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
-
         return redirect('/dashboard/post')->with('success', 'Post has been deleted!'); // Corrected the success message
     }
 
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -1,6 +1,6 @@
-    @extends('dashboard.layoutf.main')
+@extends('dashboard.layoutf.main')
 
-    @section('container')
+@section('container')
     <div class="row justify-content-center my-3">
         <div class="col-lg-8">
             <form action="/posts">
@@ -8,17 +8,27 @@
                     <input type="hidden" name="category" value="{{ request('category') }}">
                 @endif
                 @if (request('author'))
-                    <input type="hidden" name="author"   value="{{ request('author') }}">
+                    <input type="hidden" name="author" value="{{ request('author') }}">
                 @endif
                 <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
-                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-success"><span data-feather="arrow-left"></span>Back to all my post</a>
+                    <input type="text" class="form-control" placeholder="Search.." name="search"
+                        value="{{ request('search') }}">
+                    <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-success"><span
+                            data-feather="arrow-left"></span>Back to all my post</a>
                     <a href="" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
-                        <a href="" class="btn btn-danger"><span data-feather="x-circle">Delete</span>Delete</a>
+                    <a href="" class="btn btn-danger"><span data-feather="x-circle"></span>Delete</a>
                 </div>
             </form>
 
-            
+            @if ($post->image)
+                <div style="max-height: 350px; overflow:hidden">
+                    <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}"
+                        class="img-fluid mt-3">
+                </div>
+            @else
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
+                    alt="{{ $post->category->name }}" class="img-fluid mt-3">
+            @endif
         </div>
     </div>
-    @endsection
+@endsection
